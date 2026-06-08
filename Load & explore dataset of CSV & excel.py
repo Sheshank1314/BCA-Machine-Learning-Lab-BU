7cf02506ced1e56ca82d1e1da615689709c3c23d
Sheshank Bandarupalli <151662017+Sheshank1314@users.noreply.github.com>
2026-06-08 10:02:06 +0530

    Update Load & explore dataset of CSV & excel.py

--- Load & explore dataset of CSV & excel.py
+++ Load & explore dataset of CSV & excel.py
@@ -1,8 +1,27 @@
+Step 1: Creating CSV and Excel Files with Dummy Data 
+• Create CSV File: Open a text editor like Notepad or any other code editor. Enter the following data 
+Name, Age, Score
+Srikanth, 28,85
+Snigdha, 22,78
+Mary, 31,92
+Save this file as sample_data.csv in the C:\ML_Projects directory.
+
+• Create Excel File: We can use Microsoft Excel or Google Sheets to create this file. Enter the below data: 
+Name      Course      Sem
+Rajesh     BCA         1
+Ramesh     BCA         2
+Swati      BCOM        1
+Florina    BCOM        3
+Pooja      BBA         2
+Raghu      BBA         4
+Save this file as sample_data.xlsx in the C:\ML_Projects directory. 
+
+Step 2: Python Code to Load and Explore the Data
 import pandas as pd
 
 # Define the file paths
-csv_file_path = 'C:\\Users\\shesh\\CAREER\\SBAB\\BCA 3RD YEAR\\BCA 6TH SEM\\Lab Programs\\ML_Programs\\sample_data.csv'
-excel_file_path = 'C:\\Users\\shesh\\CAREER\\SBAB\\BCA 3RD YEAR\\BCA 6TH SEM\\Lab Programs\\ML_Programs\\sample_data.xlsx'
+csv_file_path = 'C:\\Users\\User\\ML_Programs\\sample_data.csv'
+excel_file_path = 'C:\\Users\\User\\ML_Programs\\sample_data.xlsx'
 
 # Load the CSV file
 data_csv = pd.read_csv(csv_file_path)
